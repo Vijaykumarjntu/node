a7e8eb9008446d92e9b85127e4a2b4a61487972b
vijay kumar <jntuvijaykumar@gmail.com>
2022-08-03 20:49:06 +0530

    env file creation

--- myApp.js
+++ myApp.js
@@ -1,8 +1,16 @@
+require("dotenv").config();
 let express = require('express');
 let app = express();
+let dj = express.static(__dirname+ "/public")
+app.use("/public",dj)
+app.get("/", function(req, res) {
+    res.sendFile(__dirname +  "/views/index.html");
+  });
+app.get("/json",function(req,res){
+  const vj = process.env.MESSAGE_STYLE === "uppercase" ? "HELLO JSON" : "Hello json";
+  res.json({message:vj});
 
-
-
+})
 
 
 
